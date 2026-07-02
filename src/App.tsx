654a349ec0cd258978fc87f98fed33ff46e802b5
sagarhasan273 <sagarhasan273@gmail.com>
2026-07-02 18:29:55 +0600

    feat: integrate question registry and enhance scrollbar styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LevelProvider } from "./context/LevelProvider";
 import { Navigation } from "./components/Navigation";
-import { Box } from "@mui/material";
+import { Box, GlobalStyles } from "@mui/material";
 import { Home } from "./pages/home/home";
 import { Feedback } from "./pages/feedback/feedback";
 import { useState } from "react";
+import { questionRegistry } from "./data/questions/registry";
 
 function App() {
   const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
@@ -21,49 +22,56 @@ function App() {
   return (
     <LevelProvider>
       <BrowserRouter>
+        <GlobalStyles
+          styles={{
+            '*::-webkit-scrollbar': {
+              display: 'none',
+              width: 0,
+              height: 0,
+            },
+            '*': {
+              scrollbarWidth: 'none',
+              msOverflowStyle: 'none',
+            },
+            'html, body, #root': {
+              height: '100%',
+              margin: 0,
+              padding: 0,
+              overflow: 'hidden',
+            },
+          }}
+        />
         <Box sx={{ 
           display: "flex", 
           flexDirection: "column", 
-          minHeight: "100vh",
           height: "100vh",
-          maxHeight: "100vh",
           bgcolor: "grey.50",
           overflow: "hidden",
-          // Hide scrollbar for Firefox
-          scrollbarWidth: "none",
-          // Hide scrollbar for IE/Edge
-          msOverflowStyle: "none",
-          // Hide scrollbar for Chrome/Safari/Opera
-          "&::-webkit-scrollbar": {
-            display: "none",
-            width: 0,
-          },
         }}>
-          {/* Navigation - outside Routes so it appears on all pages */}
-          <Navigation onMenuClick={handleMobileSidebarToggle}
-            questionCount={0} // You can pass the actual count if needed
-             />
+          <Navigation 
+            onMenuClick={handleMobileSidebarToggle}
+            questionCount={questionRegistry.length}
+          />
           
-          {/* Content */}
           <Box sx={{ 
             display: "flex", 
             flex: 1,
             mt: 0,
             overflow: "hidden",
-            // Hide scrollbar for Firefox
-            scrollbarWidth: "none",
-            // Hide scrollbar for IE/Edge
-            msOverflowStyle: "none",
-            // Hide scrollbar for Chrome/Safari/Opera
-            "&::-webkit-scrollbar": {
-              display: "none",
-              width: 0,
-            },
+            position: "relative",
+            height: "calc(100vh - 64px)", // Subtract navigation height
           }}>
             <Routes>
-              <Route path="/" element={<Home mobileSidebarOpen={mobileSidebarOpen}
+              <Route 
+                path="/" 
+                element={
+                  <Home 
+                    mobileSidebarOpen={mobileSidebarOpen}
                     onMobileSidebarClose={handleMobileSidebarClose}
-                    onMobileSidebarToggle={handleMobileSidebarToggle} />} />
+                    onMobileSidebarToggle={handleMobileSidebarToggle}
+                  />
+                } 
+              />
               <Route path="/feedback" element={<Feedback />} />
             </Routes>
           </Box>
